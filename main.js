110223609ff2addc5c0adfcd7f8cb8fb5b2b8444
Victor Luis Moreira <105164111+Orechi@users.noreply.github.com>
2023-09-22 08:32:14 -0300

    asf

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-document.querySelector('.tecla_Pom')
+function raissa() {
 
-document.querySelector('#som_tecla_Pom').play()
+    document.querySelector('#som_tecla_Pom').play()
+}
+
+const lista = document.querySelectorAll('.tecla')
+
+let contador = 0;
+
+while (contador < 9) {
+    lista[contador].onclick = raissa;
+    contador = contador + 1;
+    console.log(contador)
+
+}
+
+
