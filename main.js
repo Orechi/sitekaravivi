90a2ac1db1688e896d00003c7ae3c3321b94fc26
Victor Luis Moreira <105164111+Orechi@users.noreply.github.com>
2023-11-09 08:23:12 -0300

    ,

--- main.js
+++ main.js
@@ -10,22 +10,22 @@ for (contador < lista.length) {
     const tecla = lista[contador];
     const classe = tecla.classList[1];
     const idAudio = `#som_${classe}`;
-    tecla.onclick = function () { 
+    tecla.onclick = function () {
         tocar(idAudio);
     }
 
-    contador = contador + 1; 
-        
-    
+    contador = contador + 1;
+
+
     tecla.onkeydown = function (evento) {
-       if (evento.code === 'Enter' || evento.code === 'Space'){
-          tecla.classList.add('ativa')
-        //console.log(evento.code === 'Enter' || evento.code === 'Space')
-    }
-    tecla.onkeyup - function () {
-        tecla.classList.remove('ativa');
+        if (evento.code === 'Enter' || evento.code === 'Space') {
+            tecla.classList.add('ativa')
+            //console.log(evento.code === 'Enter' || evento.code === 'Space')
+        }
+        tecla.onkeyup - function () {
+            tecla.classList.remove('ativa');
+
+        }
+
 
     }
-
-
-}
